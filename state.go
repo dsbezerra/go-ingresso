bca94f21a9969976b670e090503f0ae7ba9511e3
Diego Bezerra <dsouzaduarte@gmail.com>
2021-11-11 09:30:36 -0300

    Update state.go

--- state.go
+++ state.go
@@ -22,7 +22,7 @@ type City struct {
 }
 
 // GetStates Get all states and cities available
-// https://api-content.ingresso.com/v0/swagger/ui/index#!/States/States_GetAsync
+// https://api-content.ingresso.com/v0/states
 func (ing *Ingresso) GetStates() ([]State, error) {
 	var states []State
 	url := fmt.Sprintf("%sstates", ing.BaseURL)
@@ -31,7 +31,7 @@ func (ing *Ingresso) GetStates() ([]State, error) {
 }
 
 // GetState Get a single state and its cities
-// https://api-content.ingresso.com/v0/swagger/ui/index#!/States/States_GetAsync_0
+// https://api-content.ingresso.com/v0/states/{uf}
 func (ing *Ingresso) GetState(uf string) (*State, error) {
 	var state State
 	url := fmt.Sprintf("%sstates/%s", ing.BaseURL, uf)
@@ -40,7 +40,7 @@ func (ing *Ingresso) GetState(uf string) (*State, error) {
 }
 
 // GetCity Get a single city by Id
-// https://api-content.ingresso.com/v0/swagger/ui/index#!/States/States_GetCityAsync
+// https://api-content.ingresso.com/v0/states/city/{id}
 func (ing *Ingresso) GetCity(id string) (*City, error) {
 	var city City
 	url := fmt.Sprintf("%sstates/city/%s", ing.BaseURL, id)
@@ -49,10 +49,13 @@ func (ing *Ingresso) GetCity(id string) (*City, error) {
 }
 
 // GetCityURLKey Get a single city by urlKey
-// https://api-content.ingresso.com/v0/swagger/ui/index#!/States/States_GetCityByUrlKeyAsync
+// https://api-content.ingresso.com/v0/states/city/name/{urlKey}
 func (ing *Ingresso) GetCityURLKey(urlKey string) (*City, error) {
 	var city City
 	url := fmt.Sprintf("%sstates/city/name/%s", ing.BaseURL, urlKey)
 	_, err := ing.getIngresso(url, &city)
 	return &city, err
 }
+
+// TODO(diego): https://api-content.ingresso.com/v0/states/city/latlong
+// TODO(diego): https://api-content.ingresso.com/v0/states/city/theater/{theaterSlug}
